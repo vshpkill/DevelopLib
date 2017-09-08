f6581abd948e533ab6faa4165b34845baae544c0
heshufeng <heshufeng@dayima.com>
2017-09-08 16:12:36 +0800

    整理提交

--- src/main/java/develop/lib/customview/CustomView.java
+++
@@ -1,8 +0,0 @@
-package develop.lib.customview;
-
-/**
- * Created by xiaohe on 17-9-8.
- */
-
-public class CustomView {
-}
---
+++ src/main/java/develop/lib/customview/addreduceview/AddReduceView.java
@@ -0,0 +1,8 @@
+package develop.lib.customview.addreduceview;
+
+/**
+ * Created by xiaohe on 17-9-8.
+ */
+
+public class AddReduceView {
+}
